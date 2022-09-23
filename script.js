ffa8e0a2f84b2ccef1b6f7e9bb5b1a6724f29a3a
natanymariano <natanymarianopt@hotmail.com>
2022-09-23 21:20:26 +0100

    add things

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+/*var nome = prompt ("Qual seu nome?");
+alert("Bem vindo " + nome)*/
+
+var num1 = prompt("Digite um número");
+num1 = parseInt(num1)
+var num2 = prompt("Digite outro número");
+num2 = parseInt(num2)
+alert("A subtração é " + (num1 - num2))
+alert("A multiplicação é " + num1 * num2)
+alert("A divisão é " + num1 / num2)
+alert("A soma é " + (num1 + num2))
